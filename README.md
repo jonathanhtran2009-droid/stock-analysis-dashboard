e9135d309bc8c5983a9279af531d612919eb4c48
jonathanhtran2009-droid <jonathanhtran2009@gmail.com>
2026-03-11 20:42:26 -0700

    Add moving averages, correlation heatmap, percentage returns

---
+++ README.md
@@ -0,0 +1,21 @@
+# Stock Analyst Dashboard
+
+A Python tool that pulls live stock market data and returns the a moving averages graph, a correlation heatmap, and a percent return of the stock in a graph.
+
+## Features
+- Percent Returns on stocks
+- Moving averages (50 or 200 days)
+- Correlation Heatmap
+- User Input
+- Invalid ticker detection
+
+## Technologies Used
+- Python
+- yfinance API
+- pandas
+- plotly.express
+
+## How to Use
+1. Clone the repository
+2. Install dependencies: `pip install yfinance`, `pip install pandas`, `pip install plotly`
+3. Run `python analysis.py`
--- analysis.py
+++ analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 import plotly.express as px
 import yfinance as yf
 
+#Gets stock Information
 def get_stock_data(Token):
     data = yf.download(Token, period="1y")
     return data
@@ -9,6 +10,7 @@ def get_stock_data(Token):
 watchList = []
 valid_tickers = []
 
+#Asks user for what stocks
 while True:
     x = input("Input a Token (type STOP to stop) ").upper()
     if x == "STOP":
@@ -16,17 +18,43 @@ while True:
     else:
         watchList.append(x)
 
-new_df = pd.DataFrame()
-
+correlation = pd.DataFrame()
+percentage_return_df = pd.DataFrame()
 
 for Token in watchList:
     df = get_stock_data(Token)
-    if df.empty:
+    if df.empty: 
         print("Invalid Token, please try again")
     else:
-        valid_tickers.append(Token)
-        new_df[Token] = (df['Close'] - df['Close'].iloc[0]) / df['Close'].iloc[0] * 100
+        #Moving Averages
+        df = df.reset_index()
 
-new_df = new_df.reset_index()
-fig = px.line(new_df, x="Date", y=valid_tickers, title="Percentage Returns over the Year")
-fig.show()
+        df.columns = ['Date', 'Close', 'High', 'Low', 'Open', 'Volume']
+
+        df = df.sort_values(by="Date")
+
+        df['moving_average_short'] = df['Close'].rolling(window=50).mean()
+
+        df['moving_average_long'] = df['Close'].rolling(window=200).mean()
+
+        #Moving Averages Graph
+        fig = px.line(df, x=('Date'), y=['Close', 'moving_average_short', 'moving_average_long'], title=f"{Token} Stock Price over the Year")
+
+        fig.show()
+
+        #Correlation
+        correlation[Token] = df['Close']
+
+        #Percent Returns
+        valid_tickers.append(Token)
+        percentage_return_df[Token] = (df['Close'] - df['Close'].iloc[0]) / df['Close'].iloc[0] * 100
+
+#Correlation Heatmap
+correlation = correlation.corr()
+correlation_fig = px.imshow(correlation)
+correlation_fig.show()
+
+#Percent Returns Graph
+new_df = percentage_return_df.reset_index()
+fig2 = px.line(new_df, x="Date", y=valid_tickers, title="Percentage Returns over the Year")
+fig2.show()
